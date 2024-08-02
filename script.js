a9dea575f742f61b5f9a98a637e5db6141a6724a
Murfew <matthew.hurley953@gmail.com>
2024-08-02 17:59:15 -0400

    added some basic logic frame for the dialog buttons

--- script.js
+++ script.js
@@ -15,55 +15,59 @@ function Book(title, author, pages, read) {
   };
 }
 
+function addNewBookToDOM(book) {
+  const libraryContainer = document.querySelector(".library");
+
+  const bookContainer = document.createElement("div");
+  const bookTitle = document.createElement("div");
+  const bookAuthor = document.createElement("div");
+  const bookPages = document.createElement("div");
+  const bookRead = document.createElement("div");
+  const bookButtons = document.createElement("div");
+  const buttonsCheck = document.createElement("span");
+  const buttonsCached = document.createElement("span");
+  const buttonsDelete = document.createElement("span");
+  const buttonsClose = document.createElement("span");
+
+  bookContainer.classList.add("book");
+  bookTitle.classList.add("title");
+  bookAuthor.classList.add("author");
+  bookPages.classList.add("pages");
+  bookRead.classList.add("read");
+  bookButtons.classList.add("buttons");
+  buttonsCheck.classList.add("material-symbols-outlined", "check");
+  buttonsCached.classList.add("material-symbols-outlined", "cached");
+  buttonsDelete.classList.add("material-symbols-outlined", "delete");
+  buttonsClose.classList.add("material-symbols-outlined", "close");
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = "by " + book.author;
+  bookPages.textContent = book.pages + " pages";
+  bookRead.textContent = "Read: " + book.read;
+  buttonsCheck.textContent = "check";
+  buttonsCached.textContent = "cached";
+  buttonsDelete.textContent = "delete";
+  buttonsClose.textContent = "close";
+
+  bookContainer.appendChild(bookTitle);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(bookPages);
+  bookContainer.appendChild(bookRead);
+
+  bookButtons.appendChild(buttonsCheck);
+  bookButtons.appendChild(buttonsCached);
+  bookButtons.appendChild(buttonsClose);
+  bookButtons.appendChild(buttonsDelete);
+
+  bookContainer.appendChild(bookButtons);
+
+  libraryContainer.appendChild(bookContainer);
+}
+
 // Displays any and all books already in the array
 function displayLibrary() {
   myLibray.forEach((book) => {
-    const libraryContainer = document.querySelector(".library");
-
-    const bookContainer = document.createElement("div");
-    const bookTitle = document.createElement("div");
-    const bookAuthor = document.createElement("div");
-    const bookPages = document.createElement("div");
-    const bookRead = document.createElement("div");
-    const bookButtons = document.createElement("div");
-    const buttonsCheck = document.createElement("span");
-    const buttonsCached = document.createElement("span");
-    const buttonsDelete = document.createElement("span");
-    const buttonsClose = document.createElement("span");
-
-    bookContainer.classList.add("book");
-    bookTitle.classList.add("title");
-    bookAuthor.classList.add("author");
-    bookPages.classList.add("pages");
-    bookRead.classList.add("read");
-    bookButtons.classList.add("buttons");
-    buttonsCheck.classList.add("material-symbols-outlined", "check");
-    buttonsCached.classList.add("material-symbols-outlined", "cached");
-    buttonsDelete.classList.add("material-symbols-outlined", "delete");
-    buttonsClose.classList.add("material-symbols-outlined", "close");
-
-    bookTitle.textContent = book.title;
-    bookAuthor.textContent = "by " + book.author;
-    bookPages.textContent = book.pages + " pages";
-    bookRead.textContent = "Read: " + book.read;
-    buttonsCheck.textContent = "check";
-    buttonsCached.textContent = "cached";
-    buttonsDelete.textContent = "delete";
-    buttonsClose.textContent = "close";
-
-    bookContainer.appendChild(bookTitle);
-    bookContainer.appendChild(bookAuthor);
-    bookContainer.appendChild(bookPages);
-    bookContainer.appendChild(bookRead);
-
-    bookButtons.appendChild(buttonsCheck);
-    bookButtons.appendChild(buttonsCached);
-    bookButtons.appendChild(buttonsClose);
-    bookButtons.appendChild(buttonsDelete);
-
-    bookContainer.appendChild(bookButtons);
-
-    libraryContainer.appendChild(bookContainer);
+    addNewBookToDOM(book);
   });
 }
 
@@ -77,4 +81,28 @@ myLibray.push(
 window.onload = displayLibrary();
 
 const dialog = document.querySelector("dialog");
-dialog.showModal();
+const addBookButton = document.querySelector(".add-book");
+const confirmButton = document.querySelector(".confirm-button");
+const cancelButton = document.querySelector(".cancel-button");
+const newBookDetails = document.querySelectorAll("dialog input");
+const newBookRead = document.querySelector("dialog select");
+
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialog.addEventListener("close", () => {
+  if (dialog.returnValue === "default") {
+    myLibray.push(newBook);
+    addNewBookToDOM(newBook);
+  }
+});
+
+confirmButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  dialog.close(confirmButton.value);
+});
+
+cancelButton.addEventListener("click", () => {
+  dialog.close();
+});
